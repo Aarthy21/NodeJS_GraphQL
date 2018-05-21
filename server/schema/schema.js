0536799de908965e23da86b3f169816afdbec464
ikismail <ikismail7@gmail.com>
2018-05-21 23:53:02 +0530

    updated books type

--- server/schema/schema.js
+++ server/schema/schema.js
@@ -25,8 +25,8 @@ var books = [
 	{id:"2",name: "The Final Empire", genre: "Fantasy", authorId: "2"},
 	{id:"3",name: "The Long Earth", genre: "Sci-Fi", authorId: "3"},
 	{id:"4",name: "The Hero of Ages", genre: "Fantasy", authorId: "2"},
-	{id:"5",name: "The Colour of Magic", genre: "Fantasy", authorId: "3"},
-	{id:"6",name: "The Light Fantastic", genre: "Fantasy", authorId: "3"}
+	{id:"5",name: "The Colour of Magic", genre: "Fantasy", authorId: "1"},
+	{id:"6",name: "The Light Fantastic", genre: "Fantasy", authorId: "1"}
 ];
 
 var authors = [
